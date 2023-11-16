318d16d8a579e9adde0973a62d83c3bc56010072
duongltsbd1 <150758102+duongltsbd1@users.noreply.github.com>
2023-11-16 14:56:08 +0700

    fix

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from netmiko import ConnectHandler
 import telnetlib
 import socket
 import pandas as pd
-
 import numpy as np
 import random
 import requests
@@ -11,6 +10,9 @@ import time
 import json
 import re
 from sys import path
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 st.set_page_config(
     page_title="MyNet",
     page_icon="🧊",
@@ -22,7 +24,7 @@ st.set_page_config(
         'About': "# :rainbow[Made by me!] :sparkles:"
     }
 )
-path.append('C:\\Users\\Admin\\Desktop\\python\\mynet\\info')
+path.append('mynet\\info')
 def take_info(file):
     device_str=open(file,"r").read()
     device_str_p=device_str.replace("'","\"")
@@ -46,7 +48,7 @@ def take_info_device(ip_list):
     device_list=[]
     modu=[]
     for ip in ip_list:
-        file="C:\\Users\\Admin\\Desktop\\python\\mynet\\info/"+ip+".txt"
+        file="info/"+ip+".txt"
         device= take_info(file)
         device_list.append(device["device"])
     for device in device_list:
@@ -65,24 +67,79 @@ def take_info_device(ip_list):
             modu.append("")
             connection.disconnect()
             continue
-    open("C:\\Users\\Admin\\Desktop\\python\\mynet\\info/sta_list.txt","w").write("")
-    open("C:\\Users\\Admin\\Desktop\\python\\mynet\\info/vers_list.txt","w").write("")
-    open("C:\\Users\\Admin\\Desktop\\python\\mynet\\info/modul_list.txt","w").write("")
+    open("info/sta_list.txt","w").write("")
+    open("info/vers_list.txt","w").write("")
+    open("info/modul_list.txt","w").write("")
     for i in range(len(sta)):
-        open("C:\\Users\\Admin\\Desktop\\python\\mynet\\info/sta_list.txt","a").write(str(sta[i])+"\n")
-        open("C:\\Users\\Admin\\Desktop\\python\\mynet\\info/vers_list.txt","a").write(str(vers[i])+"\n")
-        open("C:\\Users\\Admin\\Desktop\\python\\mynet\\info/modul_list.txt","a").write(str(modu[i])+"\n")
+        open("info/sta_list.txt","a").write(str(sta[i])+"\n")
+        open("info/vers_list.txt","a").write(str(vers[i])+"\n")
+        open("info/modul_list.txt","a").write(str(modu[i])+"\n")
     return sta,vers,modu
+def get_score(str1,list_str,list_md):
+    list_score=[]
+    for i_1 in list_str:    
+        if str1.startswith("WS-C"):
+            if str1[4:8] in i_1:
+                score=1000
+            else:
+                score=0
+        else:
+            score=0
+        dup=0
+        for i_2 in range(len(str1)):
+            up=1
+            add_sc=1
+            check=str1[i_2] in i_1
+            if check:
+                dup+=1
+            while check:
+                up+=1
+                add_sc+=1
+                check=str1[i_2:i_2+up] in i_1
+                if i_2+up > len(str1):
+                    check=False
+            score+=add_sc
+        list_score.append(score*dup)
+    max=list_score[0]
+    index_max=0
+    for i in range(len(list_str)):
+        if list_score[i]>max:
+            max=list_score[i]
+            index_max=i
+    return list_score[index_max],list_md[index_max]
+def get_recom_ver(modu_list):
+    recom_vers=[]
+    router=open("info/router_concept.txt","r").read().split("\n")
+    router_md=open("info/router_ID.txt","r").read().split("\n")
+    switch=open("info/switch_concept.txt","r").read().split("\n")
+    switch_md=open("info/switch_ID.txt","r").read().split("\n")
+    for i_1 in modu_list:
+        if i_1 !="":
+            router_sc,mdfId_r=get_score(i_1,router,router_md)
+            sw_sc,mdfId_s=get_score(i_1,switch,switch_md)
+            if router_sc > sw_sc:
+                mdfId=mdfId_r
+            else:
+                mdfId=mdfId_s
+            driver = webdriver.Chrome()
+            url_ver="https://software.cisco.com/download/home/"+mdfId+"/type/280805680"
+            driver.get(url_ver)
+            time.sleep(2)
+            content_get=driver.find_element(By.CSS_SELECTOR, 'span.selectedRelease')
+            recom_vers.append(content_get.text)
+        else:
+            recom_vers.append("")
+    return recom_vers
 st.title('dashboard')
 hide=st.empty()
-device_list=open("C:\\Users\\Admin\\Desktop\\python\\mynet\\info/device_list.txt","r").read().split("\n")
+device_list=open("info/device_list.txt","r").read().split("\n")
 device_list.remove("")
-ip_list=open("C:\\Users\\Admin\\Desktop\\python\\mynet\\info/ip_list.txt","r").read().split("\n")
+ip_list=open("info/ip_list.txt","r").read().split("\n")
 ip_list.remove("")
 total=len(device_list)
-up_de=int(open("C:\\Users\\Admin\\Desktop\\python\\mynet\\info/up_de.txt","r").read())
+up_de=int(open("info/up_de.txt","r").read())
 hide.metric("total device:", total, up_de)
-sta_l=open("C:\\Users\\Admin\\Desktop\\python\\mynet\\info/sta_list.txt","r").read().split("\n")
+sta_l=open("info/sta_list.txt","r").read().split("\n")
 del(sta_l[-1])
 sta=[]
 for i in sta_l:
@@ -90,10 +147,11 @@ for i in sta_l:
         sta.append(True)
     else:
         sta.append(False)
-vers=open("C:\\Users\\Admin\\Desktop\\python\\mynet\\info/vers_list.txt","r").read().split("\n")
+vers=open("info/vers_list.txt","r").read().split("\n")
 del(vers[-1])
-modu=open("C:\\Users\\Admin\\Desktop\\python\\mynet\\info/modul_list.txt","r").read().split("\n")
+modu=open("info/modul_list.txt","r").read().split("\n")
 del(modu[-1])
+recom_ver=get_recom_ver(modu)
 try:
     df = pd.DataFrame(
         {
@@ -101,8 +159,8 @@ try:
             "ip": ip_list,
             "status": sta,
             "version": vers,
-            "module": modu
-            #"recom_ver": recom
+            "module": modu,
+            "recom_ver": recom_ver
         }
     )
 except:
@@ -113,8 +171,8 @@ except:
             "ip": ip_list,
             "status": sta,
             "version": vers,
-            "module": modu
-            #"recom_ver": recom
+            "module": modu,
+            "recom_ver": recom_ver
         }
     )
 edited_df = st.data_editor(
@@ -126,7 +184,8 @@ edited_df = st.data_editor(
         "ip": "ip",
         "status": "status",
         "version": "current version",
-        "module": "module"
+        "module": "module",
+        "recom_ver": "recommend version"
     },
     disabled=df.columns
 )
@@ -134,7 +193,7 @@ sta,vers,modu=take_info_device(ip_list)
 while True:
     up_de_new=check_heathy(ip_list)
     if up_de != up_de_new:
-        open("C:\\Users\\Admin\\Desktop\\python\\mynet\\info/up_de.txt","w").write(str(up_de_new))
+        open("info/up_de.txt","w").write(str(up_de_new))
         up_de=up_de_new
         hide.empty()
         hide.metric("total device:", total, up_de)
